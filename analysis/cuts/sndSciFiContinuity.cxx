0618b6a8f2e6fe8360f2cd5f96619ac9bcca5fb1
Cristovao Vilela <cvilela@lxplus919.cern.ch>
2024-02-01 15:57:56 +0100

    Adding missing cut

---
+++ analysis/cuts/sndSciFiContinuity.cxx
@@ -0,0 +1,33 @@
+#include "sndSciFiContinuity.h"
+
+#include "sndSciFiTools.h"
+
+#include "TChain.h"
+
+namespace sndAnalysis{
+  sciFiContinuity::sciFiContinuity(TChain * tree) : sciFiBaseCut(tree){
+    cutName = "SciFi continuity";
+
+    shortName = "SciFiContinuity";
+    nbins = std::vector<int>{1};
+    range_start = std::vector<double>{0};
+    range_end = std::vector<double>{1};
+    plot_var = std::vector<double>{-1};
+
+  }
+
+  bool sciFiContinuity::passCut(){
+    initializeEvent();
+    
+    bool gotFirstPlane = false;
+    // For a plane to count, need both planes to have hits
+    for (int i_plane = 0; i_plane < hits_per_plane_horizontal.size(); i_plane++){
+      if ((hits_per_plane_horizontal[i_plane]*hits_per_plane_vertical[i_plane] == 0) and gotFirstPlane){
+	  return false;
+      } else if (hits_per_plane_horizontal[i_plane]*hits_per_plane_vertical[i_plane] > 0){
+	gotFirstPlane = true;
+      }
+    }
+    return true;
+  }
+}	     
